06d42891d45bcd53d8945fd6c023c6ba31b22087
cminarellineto <cminarellineto@hotmail.com>
2022-10-22 10:17:42 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// let tecladoArr = document.querySelectorAll("span");
+let botaoArr = document.querySelectorAll("button");
+let botao = botaoArr
+
+botao.addEventListener('click', function() {
+    console.log(botao.value)
+})
+
+// function darValor() {
+//     botao.forEach(span => {
+//         return Element.value
+//     });
+// }
